b84112fba2271e3d9f5fe4d763947a780f3ab718
Jose Herrera <jmhm2012@gmail.com>
2017-04-06 01:59:47 -0600

    -basic twitter authorization working, try it with the log in button. -note: auth only works locally for now due to using the url 0.0.0.0:3000 on twitter for redirect -I had to modify the node-twitter-api lib, please use my copy (I'll get it to you)

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var watson = require('watson-developer-cloud');
 var extend = require('util')._extend;
 var i18n = require('i18next');
 var Twitter = require('twitter');
-// Load the full build.
-var _ = require('lodash');
+var TwitterAuth = require("node-twitter-api");
+var Canvas = require("canvas");
+var cloud = require("./cloud");
+var d3Scale = require("d3-scale");
 
 var app = express();
 
@@ -22,109 +24,21 @@ require('./config/i18n')(app);
 // Bootstrap application settings
 require('./config/express')(app);
 
-
-var Canvas = require("canvas");
-var cloud = require("./cloud");
-var d3Scale = require("d3-scale");
-
-
-function calculateWordSizes(words){
-	words.sort();
-	var prevWord = words[0];
-	var countPrevWord = 1;
-	var wordsWithCounts = [];
-	for( var i = 1; i < words.length; i++){
-		if(words[i] == prevWord){
-			countPrevWord ++;
-		}
-		else{
-			wordsWithCounts.push({text: prevWord, count: countPrevWord});
-			prevWord = words[i];
-			countPrevWord = 1;
-		}
-		
-		if(i == words.length -1)
-			wordsWithCounts.push({text: prevWord, count: countPrevWord});
-	}
-	var largestCount = -1;
-	var smallestCount = Infinity;
-	for(var i in wordsWithCounts){
-		if(wordsWithCounts[i].count > largestCount)
-			largestCount = wordsWithCounts[i].count;
-		if(wordsWithCounts[i].count < smallestCount)
-			smallestCount = wordsWithCounts[i].count;
-	}
-	var smallestFont = 15;
-	var largestFont = 100;
-	var m = ( largestFont - smallestFont) / (largestCount - smallestCount);
-	var b = largestFont - (m * largestCount);
-	for(var i in wordsWithCounts)
-	{
-		wordsWithCounts[i].size = (m * wordsWithCounts[i].count + b);
-	}
-	return wordsWithCounts;
-}
-
-function genSVG(words, callback){
-	
-	wordsWithSizes = calculateWordSizes(words);
-	
-	
-	var convertToSVGString = function (svgAttr){
-		var fill = d3Scale.scaleOrdinal(d3Scale.schemeCategory20);
-		var svg = "";
-		svg += "<svg width='960' height='500' version='1.1' xmlns='http://www.w3.org/2000/svg'>";
-		svg += "<g transform='translate(480,250)'>";
-		for(var i in svgAttr){
-			var text = svgAttr[i];
-			svg +="<text text-anchor='middle' transform='translate(" +text.x + "," 
-				+ text.y + ")rotate(" + text.rotate + ")' style='font-size: " 
-				+ text.size + "px; font-family: Impact; fill: " + fill(i) + ";'>";
-			svg += text.text;
-			svg +="</text>"
-		}
-		svg += "</g>";
-		svg += "</svg>";
-		callback(svg);
-	}
-
-	cloud().size([960, 500])
-		.canvas(function() { return new Canvas(1, 1); })
-		.words(wordsWithSizes)
-		.padding(5)
-		.rotate(function() { return (Math.random()*150) - 75; })
-		.font("Impact")
-		.fontSize(function(d) { return d.size; })
-		.on("end", convertToSVGString)
-		.start();
-}
-
-function stringToWords(string){
-	var lowerCaseTxt = string.toLowerCase();
-	var words = lowerCaseTxt.split(/\s+/);
-	if(words[words.length - 1] === "")
-		words.pop();
-	for(var i in words){
-		words[i] =  words[i].replace(/[^0-9a-zA-Z#]/g, '');
-	}
-	return words;
-}
-
-app.get("/getSVG", function(req, res){
-	var callback = function(svg){
-		res.send(svg);
-	}
-	var txt = 'How the Word Cloud Generator Works \
+app.get("/getSVG", function (req, res) {
+  var callback = function (svg) {
+    res.send(svg);
+  }
+  var txt = 'How the Word Cloud Generator Works \
 				The layout algorithm for positioning words without overlap is available on GitHub under an open source license as d3-cloud. \
 				Note that this is the only the layout algorithm and any code for converting text into words and rendering the final output requires additional development. \
 				As word placement can be quite #slow for more than a few hundred words, the layout algorithm can be run asynchronously, \
 				with a configurable time step size. This makes it possible to animate words as they are placed without stuttering. It is \
 				recommended to always use a time step even without animations as it prevents the browser’s event loop from blocking while placing the words. \
 				The layout algorithm itself is incredibly simple. For each word, starting with the most "important": '
-	var words = stringToWords(txt);
-	//console.log(words);
-	//var words = ["Hello", "Hello", "Hello", "Hello", "Hello", "Hello","world", "normally", "world","you", "want", "more","world", "world", "words", "than", "this"]
-	genSVG(words, callback);
+  var words = stringToWords(txt);
+  //console.log(words);
+  //var words = ["Hello", "Hello", "Hello", "Hello", "Hello", "Hello","world", "normally", "world","you", "want", "more","world", "world", "words", "than", "this"]
+  genSVG(words, callback);
 });
 
 
@@ -138,7 +52,11 @@ var client = new Twitter({
 });
 
 
-
+var twitterauth = new TwitterAuth({
+  consumerKey: 'nzHVwjmv85ctMPpF9rfS2OKxI',
+  consumerSecret: 't7jEvPmxTHdFJ2OXW8Q4Qjq2cBMZVWYeTJIp5AzYP8dcRnqXJj',
+  callback: "http://0.0.0.0:3000/loggedIn"
+});
 
 // Create the service wrapper
 var personalityInsights = watson.personality_insights({
@@ -233,9 +151,71 @@ function packtext(tweets, callback) {
   });
 
   callback(packedString);
-
-
 }
+
+
+app.get("/loggedIn", function (req, res) {
+  console.log("callback from twitter!");
+
+  res.sendFile('/public/loggedin.html', { root: __dirname });
+});
+
+
+
+var _requestSecret; //why is this here, isn't it keeping some kind of state?
+app.get("/request-token", function (req, res) {
+
+  twitterauth.getRequestToken(function (err, requestToken, requestSecret) {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    else {
+
+      console.log("request token: " + requestToken)
+      console.log("request secret: " + requestSecret)
+
+      _requestSecret = requestSecret;
+      res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+    }
+  });
+
+});
+
+
+app.get("/access-token", function (req, res) {
+
+  console.log("time to get the access token!")
+  var requestToken = req.query.oauth_token;
+  var verifier = req.query.oauth_verifier;
+
+  console.log("oauth request token: " + requestToken)
+  console.log("oauth verifier: " + verifier)
+
+
+  twitterauth.getAccessToken(requestToken, _requestSecret, verifier, function (err, accessToken, accessSecret) {
+    if (err)
+      res.status(500).send(err);
+    else {
+
+      console.log("access token: " + accessToken)
+      console.log("access Secret: " + accessSecret)
+
+      twitterauth.verifyCredentials(accessToken, accessSecret, function (err, user) {
+        if (err)
+          res.status(500).send(err);
+        else
+        {
+          console.log(user);
+          res.send(user);
+        }
+      });
+    }
+  });
+
+
+
+});
 
 
 
@@ -276,3 +256,86 @@ require('./config/error-handler')(app);
 var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 app.listen(port);
 console.log('listening at:', port);
+
+
+//svg functions
+function calculateWordSizes(words) {
+  words.sort();
+  var prevWord = words[0];
+  var countPrevWord = 1;
+  var wordsWithCounts = [];
+  for (var i = 1; i < words.length; i++) {
+    if (words[i] == prevWord) {
+      countPrevWord++;
+    }
+    else {
+      wordsWithCounts.push({ text: prevWord, count: countPrevWord });
+      prevWord = words[i];
+      countPrevWord = 1;
+    }
+
+    if (i == words.length - 1)
+      wordsWithCounts.push({ text: prevWord, count: countPrevWord });
+  }
+  var largestCount = -1;
+  var smallestCount = Infinity;
+  for (var i in wordsWithCounts) {
+    if (wordsWithCounts[i].count > largestCount)
+      largestCount = wordsWithCounts[i].count;
+    if (wordsWithCounts[i].count < smallestCount)
+      smallestCount = wordsWithCounts[i].count;
+  }
+  var smallestFont = 15;
+  var largestFont = 100;
+  var m = (largestFont - smallestFont) / (largestCount - smallestCount);
+  var b = largestFont - (m * largestCount);
+  for (var i in wordsWithCounts) {
+    wordsWithCounts[i].size = (m * wordsWithCounts[i].count + b);
+  }
+  return wordsWithCounts;
+}
+
+function genSVG(words, callback) {
+
+  wordsWithSizes = calculateWordSizes(words);
+
+
+  var convertToSVGString = function (svgAttr) {
+    var fill = d3Scale.scaleOrdinal(d3Scale.schemeCategory20);
+    var svg = "";
+    svg += "<svg width='960' height='500' version='1.1' xmlns='http://www.w3.org/2000/svg'>";
+    svg += "<g transform='translate(480,250)'>";
+    for (var i in svgAttr) {
+      var text = svgAttr[i];
+      svg += "<text text-anchor='middle' transform='translate(" + text.x + ","
+        + text.y + ")rotate(" + text.rotate + ")' style='font-size: "
+        + text.size + "px; font-family: Impact; fill: " + fill(i) + ";'>";
+      svg += text.text;
+      svg += "</text>"
+    }
+    svg += "</g>";
+    svg += "</svg>";
+    callback(svg);
+  }
+
+  cloud().size([960, 500])
+    .canvas(function () { return new Canvas(1, 1); })
+    .words(wordsWithSizes)
+    .padding(5)
+    .rotate(function () { return (Math.random() * 150) - 75; })
+    .font("Impact")
+    .fontSize(function (d) { return d.size; })
+    .on("end", convertToSVGString)
+    .start();
+}
+
+function stringToWords(string) {
+  var lowerCaseTxt = string.toLowerCase();
+  var words = lowerCaseTxt.split(/\s+/);
+  if (words[words.length - 1] === "")
+    words.pop();
+  for (var i in words) {
+    words[i] = words[i].replace(/[^0-9a-zA-Z#]/g, '');
+  }
+  return words;
+}
